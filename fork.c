783d6712809a7805f06bcf7a0bb045fff9dab2a3
Amedamin <ahmedqryqr@gmail.com>
2023-11-11 05:30:50 -0800

    done

--- fork.c
+++ fork.c
@@ -1,36 +1,36 @@
-#include "hsh.h" 
+#include "hsh.h"
 
-/** 
- * my_fork -handles the fork 
- * Return: Nothing If successful or error if it fails. 
- * @arg: Arguments passed to the shell program. 
- */ 
-int my_fork(char **arg) 
-{ 
-	pid_t child_ID, wpid; 
-	int ID_status; 
+/**
+ * my_fork -handles the fork
+ * Return: Nothing If successful or error if it fails.
+ * @arg: Arguments passed to the shell program.
+ */
+int my_fork(char **arg)
+{
+	pid_t child_ID, wpid;
+	int ID_status;
 
-	child_ID = fork(); 
-	if (child_ID ==  0) 
-	{ 
-		if (execvp(arg[0], arg) == -1) 
-		{ error(arg[0]); 
-			return(127); 
-		} 
-	} 
-	else if (child_ID< 0) 
-		perror("Error forking process"); 
+	child_ID = fork();
+	if (child_ID ==  0)
+	{
+		if (execvp(arg[0], arg) == -1)
+		{ error(arg[0]);
+			return(127);
+		}
+	}
+	else if (child_ID< 0)
+		perror("Error forking process");
 
-	else 
-		wpid = waitpid(child_ID, &ID_status, 0); 
+	else
+		wpid = waitpid(child_ID, &ID_status, 0);
 
-	if (wpid != -1) 
-		if (WIFEXITED(ID_status)) { 
-			int exit_status = WEXITSTATUS(ID_status); 
+	if (wpid != -1)
+		if (WIFEXITED(ID_status)) {
+			int exit_status = WEXITSTATUS(ID_status);
 
-			if (exit_status == 2) 
-				return(2); 
-			else if (exit_status == 127)  
-				return(127); } 
-	return (-1); 
+			if (exit_status == 2)
+				return(2);
+			else if (exit_status == 127) 
+				return(127); }
+	return (-1);
 }
--- free.c
+++ free.c
@@ -1,14 +1,14 @@
-nclude "hsh.h" 
-/** 
- * free_all - free memory 
- * @arg: Arguments passed to the shell program. 
- */ 
+#include "hsh.h"
+/**
+ * free_all - free memory
+ * @arg: Arguments passed to the shell program.
+ */
 
-void free_all(char **arg) 
-{int i; 
-	if (arg == NULL) 
-		return; 
-	for (i = 0; arg[i] != NULL; i++) 
-		free(arg[i]); 
-	free(arg); 
+void free_all(char **arg)
+{int i;
+	if (arg == NULL)
+		return;
+	for (i = 0; arg[i] != NULL; i++)
+		free(arg[i]);
+	free(arg);
 }
