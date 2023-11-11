e4accf632c974b9019a6057d8d8dbf4783da7093
AsiaEasa <asiaeasa89@gmail.com>
2023-11-11 05:39:11 -0800

    done

--- in_main.c
+++ in_main.c
@@ -12,14 +12,14 @@ int check(char **arg)
 		return (-1);
 	}
 
-	if(_strncmp(arg[0], "exit", 4) == 0)
-		return(handle_exit(arg));
+	if (_strncmp(arg[0], "exit", 4) == 0)
+		return (handle_exit(arg));
 
 	if (_strncmp(arg[0], "env", 3) == 0 || _strncmp(arg[0], "printenv", 4) == 0)
-		return(envi());
+		return (envi());
 
 	if (_strncmp(arg[0], "cd", 2) == 0)
-		return(cd(arg));
+		return (cd(arg));
 
 	return (my_fork(arg));
 }
