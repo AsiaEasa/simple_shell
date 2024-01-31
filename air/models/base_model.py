d2e28a33bb1a5a54ec7cfb626a3a22cca135ac56
AsiaEasa <asiaeasa89@gmail.com>
2024-01-31 10:19:13 -0800

    done

--- air/models/base_model.py
+++ air/models/base_model.py
@@ -3,7 +3,7 @@
 
 import uuid
 from datetime import datetime
-
+import models
 
 class BaseModel:
     """Class BaseModel"""
@@ -22,10 +22,12 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def save(self):
         """updates the public instance attribute updated_at with the current datetime"""
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """Returns a dictionary containing all keys/values of __dict__ of the instance"""
---
+++ air/models/engine/file_storage.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""Module for FileStorage class"""
+
+
+import json
+from models.base_model import BaseModel
+import models
+
+
+class FileStorage():
+    """class FileStorage that serializes instance
+	    to a JSON file and deserializes
+	    JSON file to instances
+    """
+
+    #Private class attributes:
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """returns the dictionary __objects"""
+        return self.__objects
+
+    def new(self, obj):
+        """ sets in __objects the obj with key <obj class name>.id"""
+        K = obj.__class__.__name__
+        ID = obj.id
+        self.__objects["{}.{}".format(K, ID)] = obj
+
+    def save(self):
+        """ serializes __objects to the JSON file (path: __file_path)"""
+        New = {}
+        #Use the varible New to store the objects inside
+        O = {K: V.to_dict() for K, V in self.__objects.items()}
+
+        with open(self.__file_path, 'w')as fi_1:
+            json.dump(New, fi_1)
+
+    def reload(self):
+        """ deserializes the JSON file to __objects (only if the JSON file
+        (__file_path) exists ; otherwise, do nothing. If the file doesn’t
+        exist, no exception should be raised)"""
+        try:
+            with open(self.__file_path, 'r', encoding="UTF8") as fi_2:
+                LOAD = json.load(fi_2)
+                for K, V in LOAD.items():
+                    classname = eval(V["__class__"])(**V)
+                    self.__objects[K] = classname
+        except FileNotFoundError:
+            pass
