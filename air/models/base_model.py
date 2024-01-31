28811e6b9f799defa5b866d5c2b73ecbd871d7d5
Amedamin <ahmedqryqr@gmail.com>
2024-01-31 05:47:41 -0800

    g

--- air/models/base_model.py
+++ air/models/base_model.py
@@ -6,35 +6,38 @@ from datetime import datetime
 
 
 class BaseModel:
-    """Class BaseModel"""
-    def __init__(self, *args, **kwargs):
-        """Constructor"""
-        IOS = "%Y-%m-%dT%H:%M:%S.%f"
-        if kwargs and kwargs != {}:
-            for K, V in kwargs.items():
-                if K == "created_at" or K == "updated_at":
-                    self.__dict__[k] = datetime.strptime(V, IOS)
-                if K != "__class__":
-                    setattr(self, K, V)
+	"""Class BaseModel"""
+		def __init__(self, *args, **kwargs):
+			"""Constructor"""
+				IOS = "%Y-%m-%dT%H:%M:%S.%f"
+				if kwargs and kwargs != {}:
+				for key in kwargs:
+				if key == "created_at":
+				self.__dict__["created_at"] = datetime.strptime(
+						kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+				elif key == "updated_at":
+				self.__dict__["updated_at"] = datetime.strptime(
+						kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+				else:
+				self.__dict__[key] = kwargs[key]
+				else:
+	self.id = str(uuid.uuid4())
+	self.created_at = datetime.now()
+self.updated_at = datetime.now()
 
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+	def save(self):
+	"""updates the public instance attribute updated_at with the current datetime"""
+		self.updated_at = datetime.now()
 
-    def save(self):
-        """updates the public instance attribute updated_at with the current datetime"""
-        self.updated_at = datetime.now()
+		def to_dict(self):
+	"""Returns a dictionary containing all keys/values of __dict__ of the instance"""
+		dict_C = self.__dict__.copy()
+		dict_C["created_at"] = self.created_at.isoformat()
+		dict_C["updated_at"] = self.updated_at.isoformat()
+		dict_C['__class__'] = self.__class__.__name__
+		return dict_C
 
-    def to_dict(self):
-        """Returns a dictionary containing all keys/values of __dict__ of the instance"""
-        dict_C = self.__dict__.copy()
-        dict_C["created_at"] = self.created_at.isoformat()
-        dict_C["updated_at"] = self.updated_at.isoformat()
-        dict_C['__class__'] = self.__class__.__name__
-        return dict_C
-
-    def __str__(self):
-        """print() by use __str__ method"""
-        Name = type(self).__name__
-        return "[{}] ({}) {}".format(Name, self.id, self.__dict__)
+		def __str__(self):
+			"""print() by use __str__ method"""
+				Name = type(self).__name__
+				return "[{}] ({}) {}".format(Name, self.id, self.__dict__)
