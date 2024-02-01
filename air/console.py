ed4859b8264a3140d703cb2f64a22b6580e1605b
AsiaEasa <asiaeasa89@gmail.com>
2024-02-01 02:31:31 -0800

    done

--- air/console.py
+++ air/console.py
@@ -6,6 +6,7 @@ Module Console interpreter
 import cmd
 from models.base_model import BaseModel
 import models
+import shlex
 
 class HBNBCommand(cmd.Cmd):
     """Command interpreter class."""
@@ -39,5 +40,22 @@ class HBNBCommand(cmd.Cmd):
                 models.storage.save()
                 print(New_instance.id)
 
+    def do_show(self, arg):
+        """Prints the string representation of an instance."""
+        args = shlex.split(arg)
+        if not args or len(args) < 1:
+            print("** class name missing **")
+	else:
+	     if args[0] not in HBNBCommand.classes:
+	         print("** class doesn't exist **")
+	     elif len(args) < 2:
+	         print("** instance id missing **")
+	     else:
+	         instance_key = "{}.{}".format(args[0], args[1])
+	         if instance_key in models.storage.all():
+	       	    print(models.storage.all()[instance_key])
+		else:
+		    print("** no instance found **")
+        return
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
