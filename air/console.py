18ad267bce41ba15e5966f36c53da78bbd48915c
AsiaEasa <asiaeasa89@gmail.com>
2024-02-01 00:27:17 -0800

    done

---
+++ air/console.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Module Console interpreter
+"""
+
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    """Command interpreter class."""
+
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """Quit command to exit the program."""
+        return True
+
+    def do_EOF(self, arg):
+        """Exit the program on EOF."""
+        print()
+        return True
+
+    def emptyline(self):
+        """Do nothing on empty line."""
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
---
+++ om/console.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Module Console
+"""
+
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    """HBNB Class """
+    prompt = '(hbnb) '
+    def do_quit(self, argument):
+        """Defines quit option"""
+        return True
+
+    def do_EOF(self, argument):
+        """ Defines EOF option"""
+        print()
+        return True
+
+    def emptyline(self):
+        """ Defines Empty option"""
+        pass
+if __name__ == '__main__':
+    """infinite loop"""
+    HBNBCommand().cmdloop()
