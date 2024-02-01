ab8076652b883302951258fafb6c28ad840a4025
AsiaEasa <asiaeasa89@gmail.com>
2024-02-01 01:42:33 -0800

    done

--- air/console.py
+++ air/console.py
@@ -4,11 +4,14 @@ Module Console interpreter
 """
 
 import cmd
+from models.base_model import BaseModel
+import models
 
 class HBNBCommand(cmd.Cmd):
     """Command interpreter class."""
 
     prompt = "(hbnb) "
+    classes = {'BaseModel': BaseModel}
 
     def do_quit(self, arg):
         """Quit command to exit the program."""
@@ -22,6 +25,19 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """Do nothing on empty line."""
         pass
+
+    def do_create(self, arg):
+        """Create a new instance of classes, save it, and print the id."""
+        if not arg:
+            print("** class name missing **")
+        else:
+            if arg not in HBNBCommand.classes:
+                print("** class doesn't exist **")
+            else:
+                _class = self.classes.get(arg)
+                New_instance = _class()
+                models.storage.save()
+                print(New_instance.id)
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
--- om/console.py
+++ om/console.py
@@ -4,10 +4,15 @@ Module Console
 """
 
 import cmd
+from models.base_model import BaseModel
+import sys
+import models
 
 class HBNBCommand(cmd.Cmd):
     """HBNB Class """
     prompt = '(hbnb) '
+    classes = {'BaseModel': BaseModel}
+
     def do_quit(self, argument):
         """Defines quit option"""
         return True
@@ -20,6 +25,21 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """ Defines Empty option"""
         pass
+
+    def do_create(self, argument):
+        """Creates an instance of BaseModel"""
+        if argument:
+            if argument in self.classes:
+                get_class = getattr(sys.modules[__name__], argument)
+                instance = get_class()
+                print(instance.id)
+                models.storage.save()
+            else:
+                print("** class doesn't exist **")
+        else:
+            print("** class name missing **")
+        return
+
 if __name__ == '__main__':
     """infinite loop"""
     HBNBCommand().cmdloop()
